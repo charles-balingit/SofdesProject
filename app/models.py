fa06ae85ea4c95ce250515b07dbfa972256c90cf
charles-balingit <charles.balingit28@gmail.com>
2026-03-22 12:25:25 +0800

    Update models.py

--- app/models.py
+++ app/models.py
@@ -8,10 +8,5 @@ def load_user(user_id):
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(150), unique=True)
-    password = db.Column(db.String(150))
-
-    firstname = db.Column(db.String(150))
-    lastname = db.Column(db.String(150))
-    email = db.Column(db.String(150))
-    vehicle_type = db.Column(db.String(150))
+    username = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(200))
