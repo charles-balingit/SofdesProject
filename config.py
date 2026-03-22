7d53f6108346c13f7e90ea4cdab544514bbf0379
charles-balingit <charles.balingit28@gmail.com>
2026-03-22 11:48:22 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,6 @@
+import os
+
 class Config:
     SECRET_KEY = "secret-key-123"
-    SQLALCHEMY_DATABASE_URI = "sqlite:///users.db"
+    SQLALCHEMY_DATABASE_URI = os.environ.get("https://allloykgdppuballvyan.supabase.co")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
