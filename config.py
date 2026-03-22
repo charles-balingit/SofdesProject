d9b96d00d93f508d4bb8e48d3d150b168fe80b79
charles-balingit <charles.balingit28@gmail.com>
2026-03-22 12:41:44 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,4 @@
-import os
-
-BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-
 class Config:
-    SECRET_KEY = os.getenv("SECRET_KEY", "secret-key-123")
-
-    SQLALCHEMY_DATABASE_URI = os.getenv(
-        "https://allloykgdppuballvyan.supabase.co",
-        "sqlite:///" + os.path.join(BASE_DIR, "users.db")
-    )
-
+    SECRET_KEY = "secret-key-123"
+    SQLALCHEMY_DATABASE_URI = "sqlite:///users.db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
