699f2e948c43fc180aae2213677a70f7d0c4ddd2
charles-balingit <charles.balingit28@gmail.com>
2026-03-22 16:00:56 +0800

    onnect database cc

--- config.py
+++ config.py
@@ -1,4 +1,14 @@
+import os
+
 class Config:
+<<<<<<< HEAD
     SECRET_KEY = "secret-key-123"
     SQLALCHEMY_DATABASE_URI = "sqlite:///users.db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+=======
+    SECRET_KEY = "toyota_secret_key"
+
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+>>>>>>> 9e9d8f7 (connect database)
