942d64cdb02c7b4934169e25225d8877630d6aa6
charles-balingit <charles.balingit28@gmail.com>
2026-03-22 17:55:55 +0800

    Update config.py

--- config.py
+++ config.py
@@ -2,6 +2,10 @@ import os
 
 class Config:
     SECRET_KEY = "secret-key-123"
-    SQLALCHEMY_DATABASE_URI = "sqlite:///users.db"
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
+    SQLALCHEMY_ENGINE_OPTIONS = {
+        "connect_args": {
+            "sslmode": "require"
+        }
+    }
