f23dbc42f9eb3b85c43d4f6906a8e0c8d605f504
charles-balingit <charles.balingit28@gmail.com>
2026-03-22 13:29:28 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,4 @@
-import os
-
 class Config:
-    SECRET_KEY = "toyota_secret_key"
-
-    SQLALCHEMY_DATABASE_URI = os.getenv(
-        "DATABASE_URL",
-        "sqlite:////tmp/users.db"   # ✅ Vercel writable location
-    )
-
+    SECRET_KEY = "secret-key-123"
+    SQLALCHEMY_DATABASE_URI = "sqlite:///users.db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
