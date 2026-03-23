51ca96d8506ced3cae6430563b60bae034509137
charles-balingit <charles.balingit28@gmail.com>
2026-03-23 22:59:56 +0800

    fix signup email

--- app/models.py
+++ app/models.py
@@ -10,3 +10,4 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(200))
+    email = db.Column(db.String(120), unique=True, nullable=False)
