b270f7e7f993563806f1503f0ba77e429bf85e9b
charles-balingit <charles.balingit28@gmail.com>
2026-03-26 18:05:07 +0800

    update profile

--- app/models.py
+++ app/models.py
@@ -7,6 +7,7 @@ def load_user(user_id):
 
 
 class User(db.Model, UserMixin):
+    __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(200))
