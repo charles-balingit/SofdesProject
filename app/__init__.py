0da4c5be4d7f26b2c89ad3b5418c66eab463ff09
charles-balingit <charles.balingit28@gmail.com>
2026-04-08 12:30:00 +0800

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -12,6 +12,7 @@ def create_app():
 
     db.init_app(app)
     login_manager.init_app(app)
+    login_manager.login_view = 'main.login'
 
     from .routes import main
     app.register_blueprint(main)
